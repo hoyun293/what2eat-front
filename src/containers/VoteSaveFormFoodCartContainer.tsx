db639857423710b694e8e902ef43374108bf132e
junho <jhnam@howtobiz.com>
2020-03-11 21:24:38 +0900

    fix: design qa 투표 담기

--- src/containers/VoteSaveFormFoodCartContainer.tsx
+++ src/containers/VoteSaveFormFoodCartContainer.tsx
@@ -234,7 +234,7 @@ const VoteSaveFormFoodCartContainer: React.FC<IOwnProps & IStateProps & IDispatc
       >
         <div>
           <div className='cart-list-modal__header flex-center text-xl black text-bold'>추가한 투표지</div>
-          <div className='flex-col'>
+          <div className='flex-col min-h-100 max-h-half-screen overflow-auto'>
             {_.map(voteForm.votePlaces, v => (
               <div className='flex justify-between py-2 px-4' key={v.placeId}>
                 <div className='flex items-center'>
@@ -253,7 +253,7 @@ const VoteSaveFormFoodCartContainer: React.FC<IOwnProps & IStateProps & IDispatc
           </div>
           <div
             onClick={() => setIsShowCartModal(false)}
-            className='purple text-sm flex-center cart-list-modal__confirm-btn'
+            className='purple text-sm flex-center cart-list-modal__confirm-btn mt-4'
           >
             확인
           </div>
